00bcae73825abccfb7992c1c7fb2a9662ba53778
绘星痕 <2805137028@qq.com>
2024-01-20 20:05:44 +0800

    debug

--- task/kleins/fight.py
+++ task/kleins/fight.py
@@ -11,7 +11,7 @@ class Fight(Task):
         text = ocr((1675, 397, 1802, 439))[0]
         if (":" in text) or ("：" in text):
             self.indicate("重游进行中，不能再次开启作战。")
-            return 0
+            return False
         elif text == "完成重游":
             click(1739, 420)
             self.indicate("重游完成，领取重游奖励。")
