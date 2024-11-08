f6fd3f02819550cf2b05247bfa4cec2b40652eb2
绘星痕 <2805137028@qq.com>
2024-11-08 21:04:25 +0800

    文件结构示例更新

--- personal/bat/restart.bat
+++
@@ -1,3 +0,0 @@
-@echo Rem 电脑熄屏
-TIMEOUT /T 2
-start /d "D:\Kin-project\SucroseGameAssistant" SGA.exe
--- personal/bat/start-SGA.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-tasklist|find /i "SGA.exe"
-if %errorlevel%==0 ( 
- echo "exit"
-) else (
-start /d "D:\Kin-project\SucroseGameAssistant" SGA.exe True
-)
-@echo on
