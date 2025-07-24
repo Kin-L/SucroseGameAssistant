b2e82a680c09b7e93d26081343f9d30dfb680e56
绘星痕 <2805137028@qq.com>
2025-07-25 01:40:32 +0800

    update

--- sgapack-incre.py
+++ sgapack-incre.py
@@ -53,7 +53,7 @@ for src, drc in lis:
                 continue
             lis2.append([src, drc])
 if lis2:
-    version = "3.0.2"
+    version = "3.0.3"
     _str = f"/SGAv3-{version}-replace/"
     if not path.exists(f"release/SGAv3-{version}-replace"):
         makedirs(f"release/SGAv3-{version}-replace")
